02eb50be86ea56145dcc8474645a459b398945ef
Brouillet <philippebrouillet@icloud.com>
2023-01-10 03:37:18 +0100

    update

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -4,9 +4,6 @@ const PostModel = require("../models/post.model");
 const UserModel = require("../models/user.model");
 //const { uploadErrors } = require("../utils/errors.utils");
 const ObjectID = require("mongoose").Types.ObjectId;
-const fs = require("fs");
-const { promisify } = require("util");
-const pipeline = promisify(require("stream").pipeline);
 
 module.exports.readPost = (req, res) => {
   PostModel.find((err, docs) => {
